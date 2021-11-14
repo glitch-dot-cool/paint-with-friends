f8055d3fc16dec5e2f3d1f14c21c8a24c45c5b48
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2021-11-14 15:46:19 -0500

    connect client to backend ws server
    Co-authored-by: niceguynaum <niceguynaum@gmail>

--- public/app.js
+++ public/app.js
@@ -1,10 +1,14 @@
 const app = (s) => {
+  const socket = io.connect("http://localhost:3000");
+
   s.setup = () => {
     s.createCanvas(s.windowWidth, s.windowHeight);
+    s.background(0);
   };
 
-  s.draw = () => {
-    s.background(0);
+  s.mouseDragged = () => {
+    s.fill(40, 0, 255);
+    s.circle(s.mouseX, s.mouseY, 20);
   };
 
   s.windowResized = () => {
--- server.js
+++ server.js
@@ -7,4 +7,12 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(port, () => console.log(`server listening on port ${port}`));
+const server = app.listen(port, () =>
+  console.log(`server listening on port ${port}`)
+);
+
+const io = socket(server);
+
+io.sockets.on("connection", (socket) => {
+  console.log(`client connected: ${socket.id}`);
+});
