0faed62c9b9c58c8e6feeb9aa16361cfe5417a0d
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2022-02-12 14:45:01 -0500

    cleanup & comment initialState.js

--- public/src/initialState.js
+++ public/src/initialState.js
@@ -1,28 +1,32 @@
 import { paintProperties as p } from "./constants.js";
 
-const guiParams = {
+// common state variables used by client & server
+const commonState = {
   [p.FILL_COLOR]: "#349beb",
   [p.FILL_OPACITY]: 255,
   [p.STROKE_COLOR]: "#000000",
   [p.STROKE_OPACITY]: 255,
   [p.SIZE]: 15,
-  sizeMin: 5,
-  sizeMax: 300,
-  [p.SHAPE]: ["circle", "square"],
   [p.MIRROR_X]: false,
   [p.MIRROR_Y]: false,
 };
 
-const lfoControllableParams = [
-  p.FILL_OPACITY,
-  p.STROKE_OPACITY,
-  p.SIZE,
-  p.FILL_COLOR,
-  p.STROKE_COLOR,
-  p.X,
-  p.Y,
-];
+// server state requires initial x/y coords
+export const initialServerState = {
+  ...commonState,
+  [p.X]: 0,
+  [p.Y]: 0,
+};
 
+// client requires a few extra params for rendering UI controls
+const guiParams = {
+  ...commonState,
+  [p.SHAPE]: ["circle", "square"],
+  sizeMin: 5,
+  sizeMax: 300,
+};
+
+// params for generating LFO UI panels
 const lfoParams = {
   speed: 0.00005,
   speedMin: 0.00001,
@@ -41,24 +45,24 @@ const lfoParams = {
   brightness: 100,
 };
 
+// params that can be controlled via LFO
+const lfoControllableParams = [
+  p.FILL_OPACITY,
+  p.STROKE_OPACITY,
+  p.SIZE,
+  p.FILL_COLOR,
+  p.STROKE_COLOR,
+  p.X,
+  p.Y,
+];
+
 // add toggle controls for each available LFO target
 lfoControllableParams.forEach((param) => (lfoParams[param] = false));
 
+// full client-side state object
 export const state = {
   gui: guiParams,
   lfo1: { gui: { ...lfoParams }, value: 0 },
   lfo2: { gui: { ...lfoParams }, value: 0 },
   lfo3: { gui: { ...lfoParams }, value: 0 },
-};
-
-export const initialServerState = {
-  [p.FILL_COLOR]: "#349beb",
-  [p.FILL_OPACITY]: 255,
-  [p.STROKE_COLOR]: "#000000",
-  [p.STROKE_OPACITY]: 255,
-  [p.SIZE]: 15,
-  [p.MIRROR_X]: false,
-  [p.MIRROR_Y]: false,
-  [p.X]: 0,
-  [p.Y]: 0,
 };
