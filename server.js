c88883513ffeeea2f9d85592187af38913ac444a
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2022-02-20 16:05:06 -0500

    revert port change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import { initServerP5 } from "./serverP5.js";
 import { eventEmitter } from "./event.js";
 import { EVENTS } from "./public/src/constants.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 app.use(express.json());
