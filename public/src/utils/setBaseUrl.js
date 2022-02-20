e28d66d287ee96f9a02b83506c441218ccca01a8
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2022-02-20 16:06:59 -0500

    update prod url

--- public/src/utils/setBaseUrl.js
+++ public/src/utils/setBaseUrl.js
@@ -1,12 +1,9 @@
 export const setBaseUrl = () => {
   const api = {
     dev: "http://localhost:3000",
-    prod: "https://glitch-dot-paint.herokuapp.com",
+    prod: "https://paint.glitch.cool",
   };
-  const prodUrls = [
-    "https://glitch-dot-paint.herokuapp.com/",
-    "http://glitch-dot-paint.herokuapp.com/",
-  ];
+  const prodUrls = ["https://paint.glitch.cool"];
   const url = window.location.href;
   const isProd = prodUrls.includes(url);
 
