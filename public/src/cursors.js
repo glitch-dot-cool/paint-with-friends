5d791c9e7ce355a309a3337bdb0c8ba378899c1c
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2022-03-14 22:13:24 -0400

    generate colors from usernames

--- public/src/cursors.js
+++ public/src/cursors.js
@@ -2,7 +2,7 @@ import { dimensions, EVENTS } from "./constants.js";
 
 export const initCursors = (socket) => {
   const cursors = (s) => {
-    let color = s.color(0, 255, 0);
+    let color;
 
     s.setup = () => {
       const canvas = s.createCanvas(dimensions.width, dimensions.height);
@@ -15,6 +15,8 @@ export const initCursors = (socket) => {
         const [x, y] = data;
         // last element is username
         const username = data[data.length - 1];
+        color = s.usernameToColor(username);
+        // color = s.color(s.usernameToColor(username));
         s.drawCursor(x, y, username);
       });
     };
@@ -28,6 +30,34 @@ export const initCursors = (socket) => {
       s.stroke(0);
       s.strokeWeight(2);
       s.text(username, x + 8, y + 3);
+    };
+
+    s.usernameToColor = (username) => {
+      var colors = [
+        "#e51c23",
+        "#e91e63",
+        "#9c27b0",
+        "#673ab7",
+        "#3f51b5",
+        "#5677fc",
+        "#03a9f4",
+        "#00bcd4",
+        "#009688",
+        "#259b24",
+        "#8bc34a",
+        "#afb42b",
+        "#ff9800",
+        "#ff5722",
+      ];
+
+      let hash = 0;
+      if (username.length === 0) return hash;
+      for (let i = 0; i < username.length; i++) {
+        hash = username.charCodeAt(i) + ((hash << 5) - hash);
+        hash = hash & hash;
+      }
+      hash = ((hash % colors.length) + colors.length) % colors.length;
+      return colors[hash];
     };
   };
 
