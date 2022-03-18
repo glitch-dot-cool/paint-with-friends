975f33acdfc31f1cbfd8d91f44d9668ea32687fb
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2022-03-18 15:09:52 -0400

    conditionally hide unused paintbrush props

--- public/src/utils/initUI.js
+++ public/src/utils/initUI.js
@@ -1,5 +1,5 @@
 import { state } from "../initialState.js";
-import { GUI_GUTTER, GUI_OFFSET } from "../constants.js";
+import { GUI_GUTTER, GUI_OFFSET, paintProperties as p } from "../constants.js";
 
 export const initGuiPanels = (s, thisContext) => {
   const gui = s.createGui("paintbrush", thisContext);
@@ -20,4 +20,66 @@ export const initGuiPanels = (s, thisContext) => {
   lfo3Gui.setPosition(3 * GUI_OFFSET - 2 * GUI_GUTTER);
   lfo3Gui.addObject(state.lfo3.gui);
   lfo3Gui.collapse();
+
+  handleVisibleParams();
+};
+
+const handleVisibleParams = () => {
+  // conditionally hide irrevelant properties based on current shape selection
+  const shapeSelector = document.querySelector("#shape");
+
+  // initial configuration
+  const selectElement = shapeSelector.querySelector("select");
+  const currentSelection = selectElement.options[selectElement.selectedIndex];
+  hideUnusedProperties(currentSelection.value);
+
+  // update on changes to shape selection dropdown
+  shapeSelector.addEventListener("change", (e) => {
+    hideUnusedProperties(e.target.value);
+  });
+};
+
+const hideUnusedProperties = (shape) => {
+  switch (shape) {
+    case "line":
+      setParamVisibility(getUnusedParamsForShape(shape));
+      break;
+    case "circle":
+      setParamVisibility(getUnusedParamsForShape(shape));
+      break;
+    case "square":
+      setParamVisibility(getUnusedParamsForShape(shape));
+      break;
+  }
+};
+
+const getUnusedParamsForShape = (shape) => {
+  switch (shape) {
+    case "line":
+      return [p.FILL_COLOR, p.FILL_OPACITY, p.SIZE];
+    default:
+      return [];
+  }
+};
+
+const setParamVisibility = (hideList) => {
+  const ignore = [p.X, p.Y, p.PREV_X, p.PREV_Y];
+
+  // all params minus ignored props
+  const paintProperties = Object.values(p).filter(
+    (prop) => !ignore.includes(prop)
+  );
+
+  // hide ununsed params
+  hideList.forEach((property) => {
+    document.querySelector(`#${property}`).style.display = "none";
+  });
+
+  // iterate array diff and set display to block
+  const diff = paintProperties.filter((p) => !hideList.includes(p));
+
+  // show used params
+  diff.forEach((property) => {
+    document.querySelector(`#${property}`).style.display = "block";
+  });
 };
