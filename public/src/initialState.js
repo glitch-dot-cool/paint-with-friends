c75bbafb98ce26e1058de70c80deadb615b2d009
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2022-03-29 14:55:21 -0400

    reposition lfo color sliders to prevent layout shift

--- public/src/initialState.js
+++ public/src/initialState.js
@@ -42,8 +42,6 @@ const lfoParams = {
   amountMin: 0,
   amountMax: 100,
   amountStep: 0.01,
-  saturation: 100,
-  brightness: 100,
 };
 
 // params that can be controlled via LFO
@@ -60,6 +58,9 @@ const lfoControllableParams = [
 
 // add toggle controls for each available LFO target
 lfoControllableParams.forEach((param) => (lfoParams[param] = false));
+// position optional color sliders below checkboxes to prevent layout shift
+lfoParams.saturation = 100;
+lfoParams.brightness = 100;
 
 // full client-side state object
 export const state = {
