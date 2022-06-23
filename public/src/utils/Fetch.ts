ed33aadc2ee64cfe9cea7f313e68203afbe16708
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2022-06-23 12:27:00 -0400

    convert Fetch & initUI utils to TS

--- public/src/utils/Fetch.ts
+++ public/src/utils/Fetch.ts
@@ -3,9 +3,9 @@ import { setBaseUrl } from "./setBaseUrl.js";
 export class Fetch {
   static baseUrl = setBaseUrl();
 
-  static async get(endpoint) {
+  static async get(endpoint: string) {
     try {
-      const options = {
+      const options: RequestInit = {
         method: "GET",
         mode: "cors",
         headers: {
@@ -15,15 +15,15 @@ export class Fetch {
       const response = await fetch(`${this.baseUrl}/${endpoint}`, options);
       if (response.ok) {
         return response.json();
-      } else throw "error making GET request";
+      } else throw `error making GET request to /${endpoint}`;
     } catch (error) {
       return error;
     }
   }
 
-  static async post(endpoint, data) {
+  static async post(endpoint: string, data: Record<string, string | number>) {
     try {
-      const options = {
+      const options: RequestInit = {
         method: "POST",
         mode: "cors",
         headers: {
@@ -37,7 +37,7 @@ export class Fetch {
       if (response.ok) {
         return response;
       } else {
-        throw "error making POST request";
+        throw `error making POST request to /${endpoint}`;
       }
     } catch (error) {
       return error;
