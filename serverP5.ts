971308758e4e67982961e4b9005dfc9244ec99e2
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2022-06-26 17:12:19 -0400

    serverP5.js --> .ts

--- serverP5.ts
+++ serverP5.ts
@@ -6,16 +6,18 @@ import { initialServerState } from "./public/src/initialState.js";
 import { eventEmitter } from "./event.js";
 import { EVENTS } from "./public/src/constants.js";
 import { convertLeanPaintPropertiesToObject } from "./public/src/utils/setupPaintProperties.js";
+import { DrawUpdate } from "./public/src/types/websocket.js";
 
 export const initServerP5 = () => {
-  let paintProperties = initialServerState;
-  let canvas;
+  let paintProperties = initialServerState as DrawUpdate;
+
+  let canvas: p5.canvas | undefined;
   const typeface = p5.loadFont({
     path: "public/assets/JetBrainsMonoLight.ttf",
     family: "JetBrainsMono",
   });
 
-  function sketch(s) {
+  function sketch(s: p5) {
     eventEmitter.on(EVENTS.DRAW_UPDATE, (data) => {
       paintProperties = convertLeanPaintPropertiesToObject(data);
       updateDrawing(s, paintProperties);
@@ -32,7 +34,7 @@ export const initServerP5 = () => {
   p5.createSketch(sketch);
 
   function serializeCanvas(quality = 1) {
-    return canvas.elt.toDataURL("image/png", quality);
+    return canvas?.elt.toDataURL("image/png", quality);
   }
 
   return serializeCanvas;
